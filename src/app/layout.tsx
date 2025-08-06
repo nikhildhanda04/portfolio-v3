6aed944d7b57f062a2d5523a50a5dc9b98d8ab25
nikhil <nikhildhanda84@gmail.com>
2025-08-06 15:52:02 +0530

    project initialized

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,15 +1,22 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Poppins, Poltawski_Nowy, IBM_Plex_Mono} from "next/font/google";
 import "./globals.css";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
+const poppins = Poppins({
+  variable: "--font-poppins",
+  subsets: ["latin"],
+  weight: ["100", "200", "300", "400", "500", "600", "700", "800"]
+});
+
+const poltawski = Poltawski_Nowy({
+  variable: "--font-poltawaski",
   subsets: ["latin"],
 });
 
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
+const plex = IBM_Plex_Mono({
+  variable: "--font-plex",
   subsets: ["latin"],
+  weight: ["100", "200", "300", "400", "500", "600",]
 });
 
 export const metadata: Metadata = {
@@ -25,7 +32,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${plex.variable} ${poltawski.variable} ${poppins.variable} antialiased`}
       >
         {children}
       </body>
